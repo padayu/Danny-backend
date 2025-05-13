59478dc70b1a9156f6ba74aa90edcf614d538d50
padayu <76900071+padayu@users.noreply.github.com>
2025-05-13 13:48:02 +0300

    Update dto_generation.go

--- logic/game/dto_generation.go
+++ logic/game/dto_generation.go
@@ -7,13 +7,14 @@ import (
 func (g *Game) generateStateDto(playerInstance *Player) game.StateDto {
 
 	return game.StateDto{
-		Players:     g.generatePlayersDto(),
-		Player:      g.generatePlayerDto(playerInstance),
-		Stage:       g.stage.ToDto(),
-		WordOptions: g.wordOptions,
-		Word:        g.word,
-		ScoreA:      g.scoreA,
-		ScoreD:      g.scoreD,
+		Players:       g.generatePlayersDto(),
+		Player:        g.generatePlayerDto(playerInstance),
+		Stage:         g.stage.ToDto(),
+		CardPlacement: g.generatePlacementDto(),
+		WordOptions:   g.wordOptions,
+		Word:          g.word,
+		ScoreA:        g.scoreA,
+		ScoreD:        g.scoreD,
 	}
 }
 
@@ -24,6 +25,14 @@ func (g *Game) generatePlayersDto() []game.PlayerCommonInfoDto {
 	}
 
 	return players
+}
+
+func (g *Game) generatePlacementDto() game.CardPlacementDto {
+	cards := make([]game.PlacedCardDto, 0)
+	for _, c := range g.cardPlacement {
+		cards = append(cards, game.PlacedCardDto{Id: c.Id, PositionX: c.PositionX, PositionY: c.PositionY, Rotation: c.Rotation, Flipped: c.Flipped})
+	}
+	return game.CardPlacementDto{Cards: cards}
 }
 
 func (g *Game) generatePlayerDto(playerInstance *Player) game.PlayerInfoDto {
